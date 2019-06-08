3dedc5bfd870936be6faeda9e77f013a7370ed15
Mr.Popo <46333102+admcifirecms@users.noreply.github.com>
2019-06-08 13:14:20 +0700

    Update View Member index.php
    Perbaikan kode.

--- application/views/member/index.php
+++ application/views/member/index.php
@@ -1,50 +1,26 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title><?=$this->CI->meta_title;?></title>
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport"/>
 
-	<!-- bootstrap -->
-	<link href="<?=content_url('plugins/member/css/bootstrap.min.css')?>" rel="stylesheet" />
-	
-	<!-- font-awesome -->
-	<link href="<?=content_url('plugins/font-awesome/font-awesome.min.css')?>" rel="stylesheet" />
+	<link href="<?=content_url('plugins/member/css/bootstrap.min.css')?>" rel="stylesheet"/>
+	<link href="<?=content_url('plugins/font-awesome/font-awesome.min.css')?>" rel="stylesheet"/>
+	<link href="<?=content_url('plugins/member/css/pe-icon-7-stroke.css')?>" rel="stylesheet"/>
+	<link href="<?=content_url('plugins/select2/select2.css')?>" rel="stylesheet"/>
+	<link href="<?=content_url('plugins/tagsinput/typeahead.css')?>" rel="stylesheet" type="text/css"/>
+	<link href="<?=content_url('plugins/notifications/noty.css')?>" rel="stylesheet" type="text/css"/>
+	<link href="<?=content_url('plugins/sweetalert2/sweetalert2.css')?>" rel="stylesheet"/>
+	<link href="<?=content_url('plugins/x-editable/x-editable.css');?>" rel="stylesheet" type="text/css"/>
+	<link href="<?=content_url('plugins/datetime/bootstrap-datetimepicker.min.css');?>" rel="stylesheet" type="text/css"/>
+	<link href="<?=content_url('plugins/member/css/light-bootstrap-dashboard.css')?>" rel="stylesheet" type="text/css"/>
 
-	<!-- pe-icon-7 -->
-	<link href="<?=content_url('plugins/member/css/pe-icon-7-stroke.css')?>" rel="stylesheet" />
-	
-	<!-- select2 -->
-	<link href="<?=content_url('plugins/select2/select2.css')?>" rel="stylesheet" />
-
-	<!-- tagsinput/typeahead -->
-	<link rel="stylesheet" href="<?=content_url('plugins/tagsinput/typeahead.css')?>" type="text/css">
-
-	<!-- notifications/noty -->
-	<link rel="stylesheet" href="<?=content_url('plugins/notifications/noty.css')?>" type="text/css">
-
-	<!-- sweetalert2 css -->
-	<link rel="stylesheet" href="<?=content_url('plugins/sweetalert2/sweetalert2.css')?>">
-
-
-
-	<!-- x-editable -->
-	<link rel="stylesheet" href="<?=content_url('plugins/x-editable/x-editable.css');?>" type="text/css">
-	<!-- datetimepicker -->
-	<link rel="stylesheet" href="<?=content_url('plugins/datetime/bootstrap-datetimepicker.min.css');?>" type="text/css">
-	<!-- dashboard -->
-	<link href="<?=content_url('plugins/member/css/light-bootstrap-dashboard.css')?>" rel="stylesheet" />
-
-
-
-
-	<!-- favicon -->
 	<link rel="apple-touch-icon" sizes="16x16" href="<?=content_url('images/favicon.png')?>">
 	<link rel="shortcut icon" type="image/png" href="<?=content_url('images/favicon.png')?>">
 
-	<!-- script -->
 	<script>
 		var _MOD = '<?=$this->mod;?>';
 		var _KEY = '<?=$this->_key;?>';
@@ -59,16 +35,11 @@
 	<div class="wrapper">
 		<div class="sidebar" data-color="blue">
 			<div class="sidebar-wrapper">
-				<!-- <div class="logo">
-					<a href="#" class="simple-text"><i class="nc-icon nc-satisfied"></i> Member</a>
-				</div> -->
 				<div class="logo">
-					<a href="" class="simple-text logo-mini">
+					<a href="#" class="simple-text logo-mini">
 						<img src="<?=content_url('images/logo.png')?>" style="height:27px;">
 					</a>
-					<a href="" class="simple-text logo-normal">
-						CifireCMS
-					</a>
+					<a href="#" class="simple-text logo-normal">CifireCMS</a>
 				</div>
 				<!-- Sidebar menu -->
 				<ul class="nav">
@@ -90,12 +61,6 @@
 							<p><?=lang_line('menu_account')?></p>
 						</a>
 					</li>
-					<!-- <li class="nav-item <?=$this->CI->menu_class('setting')?>">
-						<a href="<?=member_url('setting')?>" class="nav-link">
-							<i class="nc-icon nc-settings-gear-64"></i>
-							<p><?=lang_line('menu_setting')?></p>
-						</a>
-					</li> -->
 					<li class="nav-item mobile-logoutx">
 						<a href="<?=member_url('logout')?>" class="nav-link">
 							<i class="pe-7s-power"></i>
@@ -137,50 +102,30 @@
 			<!-- CONTENT -->
 			<div class="content">
 				<div class="container-fluid">
-
-				<!-- LOAD CONTENT -->
 				<?php $this->CI->load_content(); ?>
-
 				</div>
 			</div>
 			<!-- End CONTENT -->
 		</div>
 	</div>
-
-	<script src="<?=content_url('plugins/member/js/jquery.3.2.1.min.js')?>" type="text/javascript"></script>
-	<script src="<?=content_url('plugins/member/js/popper.min.js')?>" type="text/javascript"></script>
-	<script src="<?=content_url('plugins/member/js/bootstrap.min.js')?>" type="text/javascript"></script>
-	<script src="<?=content_url('plugins/member/js/bs-custom-file-input.min.js')?>" type="text/javascript"></script>
-	<script src="<?=content_url('plugins/member/js/light-bootstrap-dashboard.js')?>" type="text/javascript"></script>
-
-	<!-- DataTables js-->
+	
+	<!-- JavaScript -->
+	<script src="<?=content_url('plugins/member/js/jquery.3.2.1.min.js')?>"></script>
+	<script src="<?=content_url('plugins/member/js/popper.min.js')?>"></script>
+	<script src="<?=content_url('plugins/member/js/bootstrap.min.js')?>"></script>
+	<script src="<?=content_url('plugins/member/js/bs-custom-file-input.min.js')?>"></script>
+	<script src="<?=content_url('plugins/member/js/light-bootstrap-dashboard.js')?>"></script>
 	<script src="<?=content_url('plugins/datatable/DataTables-1.10.18.min.js')?>"></script>
-
-	<!-- select2-->
-	<script src="<?=content_url('plugins/select2/select2.min.js')?>" type="text/javascript"></script>
-	<!-- tagsinput-->
+	<script src="<?=content_url('plugins/select2/select2.min.js')?>"></script>
 	<script src="<?=content_url('plugins/tagsinput/bootstrap-tagsinput.js')?>"></script>
-	<!-- typeahead -->
 	<script src="<?=content_url('plugins/tagsinput/typeahead.bundle.min.js')?>"></script>
-	<!-- notifications -->
-	<script type="text/javascript" src="<?=content_url('plugins/notifications/noty.min.js')?>"></script>
-	<!-- sweetalert2 -->
+	<script src="<?=content_url('plugins/notifications/noty.min.js')?>"></script>
 	<script src="<?=content_url('plugins/sweetalert2/sweetalert2.min.js')?>"></script>
-
-	<!-- x-editable -->
 	<script src="<?=content_url('plugins/x-editable/x-editable.js');?>"></script>
-
-	<!-- tinymce -->
 	<script src="<?=content_url('plugins/tinymce/tinymce.min.js');?>"></script>
-
-	<!-- datetimepicker -->
 	<script src="<?=content_url('plugins/datetime/moment.js');?>"></script>
 	<script src="<?=content_url('plugins/datetime/bootstrap-datetimepicker.js');?>"></script>
-	<!-- maskedinput -->
 	<script src="<?=content_url('plugins/maskedinput/jquery.maskedinput.min.js');?>"></script>
-
-
-	<!-- custom -->
-	<script src="<?=content_url('plugins/member/js/custom.js')?>" type="text/javascript"></script>
+	<script src="<?=content_url('plugins/member/js/custom.js')?>"></script>
 </body>
 </html>
