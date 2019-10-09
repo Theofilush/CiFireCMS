53e184e2f2dbc00ae21b66009f5dc3aff8b57167
Adiman <46333102+admcifirecms@users.noreply.github.com>
2019-10-10 01:16:27 +0700

    empty

--- application/cache/routes.php
+++
@@ -1,2 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-$route['detailpost/([a-z0-9-]+)'] = 'post/index/$1';
--- application/language/indonesia/mod/menumanager_lang.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['mod_title'] = 'Manajemen Menu';
